a908a2c8454ffb095441bec743ae6e7c00190eac
Jefwillems <jefwillems100@gmail.com>
2018-06-25 21:13:11 -0400

    init

---
+++ example/usage.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+  console.log('test');
+  dragger({ empty: '.slot', drag: '.dragger' });
+});
---
+++ index.js
@@ -0,0 +1,60 @@
+const dragger = (function() {
+  let emptyList = [];
+  let draggables = [];
+  let dragging = null;
+  let draggerClass;
+  let emptyClass;
+
+  function onDragStart() {
+    dragging = this;
+    this.className += ' hold';
+    setTimeout(() => (this.className = 'invisible'), 0);
+  }
+  function onDragEnd() {
+    // dragging = null;
+    this.className = draggerClass;
+  }
+
+  function dragOver(e) {
+    e.preventDefault();
+  }
+  function dragEnter(e) {
+    e.preventDefault();
+    this.className += ' hovered';
+  }
+  function dragLeave() {
+    this.className = emptyClass;
+  }
+  function dragDrop() {
+    this.className = emptyClass;
+    this.append(dragging);
+    dragging = null;
+  }
+
+  function init(options) {
+    draggerClass = options.drag.substring(1);
+    emptyClass = options.empty.substring(1);
+    emptyList = document.querySelectorAll(options.empty);
+    draggables = document.querySelectorAll(options.drag);
+    for (const empty of emptyList) {
+      empty.addEventListener('dragover', dragOver);
+      empty.addEventListener('dragenter', dragEnter);
+      empty.addEventListener('dragleave', dragLeave);
+      empty.addEventListener('drop', dragDrop);
+    }
+    for (const el of draggables) {
+      el.setAttribute('draggable', 'true');
+      el.addEventListener('dragstart', onDragStart);
+      el.addEventListener('dragend', onDragEnd);
+    }
+  }
+  return function(options) {
+    init(options);
+
+    return {
+      reset: newOptions => {
+        init(newOptions || options);
+      },
+    };
+  };
+})();
